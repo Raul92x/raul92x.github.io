8652a3e70cf62fb6ea19c0527466029c73193e03
Raul92x <rds92x@gmail.com>
2017-04-11 05:14:54 -0700

    added more info to website

--- script.js
+++ script.js
@@ -2,4 +2,22 @@ $(document).ready(function() {
     $('.pull-me').click(function() {
         $('.panel').slideToggle('slow');
     });
+    $('button').click(function() {
+        var toAdd = $('input[name=message]').val();
+        $('.messages').append('<div class="item">' + toAdd + '</div>');
+    });
+    $(document).on('click', '.item', function() {
+    	$(this).remove();
+    });
+
+
+  	$().hover(
+    	function(){
+       		$(this).addClass('active');
+    	},
+    	function(){
+        	$(this).removeClass('active');
+    	}
+  	);
+
 });
