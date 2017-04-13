5a7a7f90be37c0967aea0491af72020e543f476b
Raul92x <rds92x@gmail.com>
2017-04-13 04:51:05 -0700

    updated website

--- extra.js
+++ extra.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {    
     $('button').click(function() {
         var toAdd = $('input[name=message]').val();
-        $('.messages').append('<div class="item">' + toAdd + '</div>');
+        $('.messages').append('<div class="newitem">' + toAdd + '</div>');
     });
-    $(document).on('click', '.item', function() {
+    $(document).on('click', '.newitem', function() {
     	$(this).remove();
     });
 });
